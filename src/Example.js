991d1b73349166f0fba5822cf30c89275379d7f7
Ethan Kline <emkline@gmail.com>
2023-10-03 13:20:41 -0500

    fixed model

--- src/Example.js
+++ src/Example.js
@@ -2,23 +2,12 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function textwrite(w){
-  const logFileText = async file => {
-      const response = await fetch(file)
-      const text = await response.text()
-      //console.log(text)
-      document.querySelector('.blue').innerHTML=text
-  }
-  const txt = logFileText(w)
-  return txt;
-}
-
 function Example() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  //textwrite("text.txt")
+
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
@@ -27,9 +16,9 @@ function Example() {
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading{console.log(textwrite("text.txt"))}{textwrite("text.txt")}</Modal.Title>
+          <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
-        <Modal.Body className='blue'>Woohoo, you are reading this text in a modal!`</Modal.Body>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
@@ -39,11 +28,8 @@ function Example() {
           </Button>
         </Modal.Footer>
       </Modal>
-
     </>
-    
   );
 }
-
 
 export default Example;
